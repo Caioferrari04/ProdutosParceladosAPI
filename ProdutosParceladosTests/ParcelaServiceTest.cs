4e03430aae21a83073c34618335df1bf8829418c
Caioferrari04 <caioferrari0484@gmail.com>
2022-05-28 19:33:37 -0300

    Adding theory type test

--- ProdutosParceladosTests/ParcelaServiceTest.cs
+++ ProdutosParceladosTests/ParcelaServiceTest.cs
@@ -14,8 +14,8 @@ public class ParcelaServiceTest
         produtoTeste = new Produto() { Codigo = 1, Nome = "Teste", Valor = 1000.00 };
         condicaoTeste = new CondicaoPagamento() { Valor = 500.00, QtdeParcelas = 2 };
         parcelasTeste = new ConcurrentBag<Parcela>() {
-            new Parcela { NumeroParcela = 1, Valor = 505.75, TaxaJurosAoMes = 1.15 },
-            new Parcela { NumeroParcela = 2, Valor = 505.75, TaxaJurosAoMes = 1.15 }
+            new Parcela { NumeroParcela = 1, Valor = 500 },
+            new Parcela { NumeroParcela = 2, Valor = 500 }
         };
     }
 
@@ -24,5 +24,39 @@ public class ParcelaServiceTest
     {
         var parcelaService = A.Fake<ParcelaServices>();
         A.CallTo(() => parcelaService.GetListaParcelas(produtoTeste, condicaoTeste)).Returns(parcelasTeste);
+    }
+
+    [Theory]
+    [InlineData(1000.00, 2)]
+    [InlineData(1000.00, 4)]
+    [InlineData(1000.00, 10)]
+    [InlineData(2000.00, 4)]
+    [InlineData(2000.00, 8)]
+    [InlineData(2000.00, 20)]
+    public void ListarParcelas(int valorProduto, int qtdeParcelas)
+    {
+        var parcelaService = A.Fake<ParcelaServices>();
+        A.CallTo(() => parcelaService.GetListaParcelas(produtoTeste, condicaoTeste));
+
+        parcelaService = new ParcelaServices();
+        var produto = new Produto { Codigo = 1, Nome = "Produto", Valor = valorProduto };
+        var condicao = new CondicaoPagamento { Valor = valorProduto/qtdeParcelas, QtdeParcelas = qtdeParcelas };
+
+        var resultados = parcelaService.GetListaParcelas(produto, condicao);
+
+        List<bool> isResultCorrect = new List<bool>();
+
+        foreach(var resultado in resultados)
+        {
+            if (qtdeParcelas > 6)
+            {
+                isResultCorrect.Add(resultado.Valor == produto.Valor/qtdeParcelas + produto.Valor * 0.0115); 
+            } 
+            else {
+                isResultCorrect.Add(resultado.Valor == produto.Valor/qtdeParcelas);
+            }
+        }
+
+        Assert.True(isResultCorrect.All(t => t));
     }
 }
